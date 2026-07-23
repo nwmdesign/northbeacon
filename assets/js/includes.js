cb59986354ab448cc9012a3a0d0b389d70d19835
nwmdesign <nwmoore1891@gmail.com>
2026-07-23 00:42:16 -0500

    Add dynamic HTML inclusion for elements with data-include

--- assets/js/includes.js
+++ assets/js/includes.js
@@ -1,1 +1,5 @@
-
+document.querySelectorAll("[data-include]").forEach(async (element) => {
+  const name = element.dataset.include;
+  const response = await fetch(`includes/${name}.html`);
+  element.innerHTML = await response.text();
+});
