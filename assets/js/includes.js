bdd8b92705df6cfb406d50c555d8f68efd664687
nwmdesign <nwmoore1891@gmail.com>
2026-07-23 00:50:34 -0500

    Delete assets/js directory

--- assets/js/includes.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll("[data-include]").forEach(async (element) => {
-  const name = element.dataset.include;
-  const response = await fetch(`includes/${name}.html`);
-  element.innerHTML = await response.text();
-});
